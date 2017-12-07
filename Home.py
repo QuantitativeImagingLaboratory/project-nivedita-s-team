4a6f34e37d09ee401b3e5d7586dd3d9b8c464c78
nvattipalli <31747094+nvattipalli@users.noreply.github.com>
2017-12-07 14:16:10 -0600

    Update Home.py
    made changes related to filtering

--- Home.py
+++ Home.py
@@ -1,211 +1,121 @@
-from tkinter import *
-from tkinter.filedialog import askopenfilename
-from PIL import ImageTk, Image
-from Filtering import *
-from BandFilter import *
-from notch import *
-import cv2
+import numpy as np
+import math as math
 
-root = Tk()
-def browseFile():
-    browseFile.filename = askopenfilename()
-    image = Image.open(browseFile.filename)
-    image = image.resize((200, 200), Image.ANTIALIAS)
-    photo = ImageTk.PhotoImage(image)
-    image1 = Label(root, image=photo)
-    image1.image = photo
-    image1.place(x=250, y=150)
+def get_ideal_low_pass_filter( shape, cutoff,width):
 
 
+    [h, w] = shape
 
-def convert():
-    img = cv2.imread(browseFile.filename, 0)
-    filtertype=tkVarPass.get()
-    if(tkVarFilter.get()=='Normal Filter'):
-        if (tkVarPass.get()=='Butterworth Low Pass')or (tkVarPass.get()=='Butterworth High Pass'):
-            order = int(change_dropdown.orderEntry.get())
-            filterimage,mask = filtering_order(img, int( cutOffEntry.get()),filtertype,order)
-        else:
-            filterimage, mask = filtering(img, int(cutOffEntry.get()), filtertype)
-    if (tkVarFilter.get() == 'Band Filter'):
-        if (tkVarPass.get() == 'Butterworth Low Pass') or (tkVarPass.get() == 'Butterworth High Pass'):
-            order = int(change_dropdown.orderEntry.get())
-            filterimage, mask = filtering_band_filter_order(img, int(cutOffEntry.get()), int(change_dropdown.WidthEntry.get()),order,filtertype)
-        else:
-            filterimage, mask = filtering_band_filter(img, int(cutOffEntry.get()), int(change_dropdown.WidthEntry.get()),filtertype)
+    mask_image = np.zeros((h, w))
+    for i in range(h):
+        for j in range(w):
+            distance = math.sqrt((i - (h / 2)) * (i - (h / 2)) + (j - (w / 2)) * (j - (w / 2)))
+            if distance >= cutoff-(width/2) and distance <= cutoff+(width/2):
+                mask_image[i][j] = 0
+            else:
+                mask_image[i][j] = 1
 
+    return mask_image
 
-    imagejpg = Image.fromarray(filterimage)
-    imagejpg.save("output.jpg")
-    image = Image.open("output.jpg")
-    image = image.resize((200, 200), Image.ANTIALIAS)
-    photo = ImageTk.PhotoImage(image)
-    image2 = Label(root, image=photo)
-    image2.image = photo
-    image2.place(x=730, y=150)
+def get_ideal_high_pass_filter( shape, cutoff,width):
 
+    mask_image = 1 - get_ideal_low_pass_filter(shape, cutoff,width)
+    return mask_image
 
-    imagejpg = Image.fromarray(mask)
-    imagejpg.save("outputimage.jpg")
-    image = Image.open("outputimage.jpg")
-    image = image.resize((200, 200), Image.ANTIALIAS)
-    photo = ImageTk.PhotoImage(image)
-    image2 = Label(root, image=photo)
-    image2.image = photo
-    image2.place(x=950, y=150)
+def get_butterworth_low_pass_filter( shape, cutoff,order,width):
+    [h, w] = shape
+    mask_image = np.zeros((h, w))
+    for i in range(h):
+        for j in range(w):
+            distance = math.sqrt((i - (h / 2)) ** 2 + ((j - (w / 2)) ** 2))
+            if distance ** 2 - cutoff ** 2 == 0:
+                mask_image[i][j] = 0
+            else:
+                mask_image[i][j] = 1 / (1 + (((distance * width) / (distance ** 2 - cutoff ** 2)) ** (2 * order)))
+    # cv2.imshow("ButterLow", mask_image)
+    # cv2.waitKey(0)
+    return mask_image
 
-    #cutOffEntry.pack()
-def notch():
-    img = cv2.imread(browseFile.filename, 0)
-    filterimage,mask=notch_filter(img)
-    imagejpg = Image.fromarray(filterimage)
-    imagejpg.save("output.jpg")
-    image = Image.open("output.jpg")
-    image = image.resize((200, 200), Image.ANTIALIAS)
-    photo = ImageTk.PhotoImage(image)
-    image2 = Label(root, image=photo)
-    image2.image = photo
-    image2.place(x=730, y=150)
+def get_butterworth_high_pass_filter( shape, cutoff,order,width):
 
-    imagejpg = Image.fromarray(mask)
-    imagejpg.save("outputimage.jpg")
-    image = Image.open("outputimage.jpg")
-    image = image.resize((200, 200), Image.ANTIALIAS)
-    photo = ImageTk.PhotoImage(image)
-    image2 = Label(root, image=photo)
-    image2.image = photo
-    image2.place(x=950, y=150)
+    mask_image = 1 - get_butterworth_low_pass_filter(shape, cutoff,order,width)
+    return mask_image
 
+def get_gaussian_low_pass_filter(shape, cutoff,width):
+    [h, w] = shape
 
-def saveImage():
-    print('saving image')
+    mask_image = np.zeros((h, w))
+    for i in range(h):
+        for j in range(w):
+            distance = math.sqrt((i - (h / 2)) ** 2 + ((j - (w / 2)) ** 2))
+            if (distance == 0):
+                mask_image[i][j] = 0
+            else:
+                mask_image[i][j] = 1 - math.exp(-(((distance ** 2 - cutoff ** 2) / (distance * width)) ** 2))
+    return mask_image
 
+def get_gaussian_high_pass_filter(shape, cutoff,width):
+    mask_image = 1 - get_gaussian_low_pass_filter(shape, cutoff,width)
+    return mask_image
 
-#window size
-root.geometry('1200x700')
+def post_process_image(image):
 
-# #placement of image
-photo = PhotoImage(file="200x200.png")
-image1= Label(root,image=photo)
-image1.place(x=250,y=150)
+    c_min = np.min(image)
+    c_max = np.max(image)
+    new_min = 0
+    new_max = 255
+    stretch_image = np.zeros((np.shape(image)), dtype=np.uint8)
+    for i in range(0, image.shape[0]):
+        for j in range(0, image.shape[1]):
+            stretch_image[i][j] = (image[i][j] - c_min) * ((new_max - new_min) / (c_max - c_min)) + new_min
 
-#placement of the changed image
-image2= Label(root,image=photo)
-image2.place(x=730,y=150)
+    return stretch_image
 
-#title details
-title = Label(root, text="FREQUENCY FILTERING", bg="white", fg="black")
-#placing the title top center
-title.place(x=540, y=0)
+def filtering_band_filter(image,cutoff,width,filtertype):
+    s = image.shape
 
-browseImage =Button(root,text="Browse Image", bg="white",fg="black",command=browseFile)
-browseImage.place(x=300,y=400)
+    fft_image = np.fft.fft2(image)
+    shift_image = np.fft.fftshift(fft_image)
+    dft_image = np.uint8(np.log(np.absolute(shift_image)) * 10)
 
+    if filtertype=='Ideal Low Pass' :
+        mask = get_ideal_low_pass_filter(s, cutoff,width)
+    elif filtertype=='Ideal High Pass' :
+        mask=get_ideal_high_pass_filter(s, cutoff,width)
+    elif filtertype=='Gaussain Low Pass':
+        mask=get_gaussian_low_pass_filter(s,cutoff,width)
+    elif filtertype=='Gaussian High Pass':
+        mask=get_gaussian_high_pass_filter(s,cutoff,width)
+    elif filtertype=='Butterworth Low Pass':
+        mask=get_butterworth_low_pass_filter(s,cutoff,width,order=2)
+    else:
+        mask=0
+    filter_image = shift_image * mask
+    filter_finalimg =np.uint8(np.log(np.absolute(filter_image))*10)
 
-#saveImage = Button(root,text="Save Image", bg="white", fg="black",command=saveImage)
-#saveImage.place(x=800,y=400)
+    ishift_image = np.fft.ifftshift(filter_image)
+    ifft_image = np.fft.ifft2(ishift_image)
+    mag_image = np.absolute(ifft_image)
+    f = post_process_image(mag_image)
 
-convertImage = Button(root,text="Click here\nto Convert", bg="white", fg="black",command=convert)
-convertImage.place(x=565,y=200)
+    return [f,filter_finalimg]
+def filtering_band_filter_order(image,cutoff,order,width,filtertype):
+    s = image.shape
 
+    fft_image = np.fft.fft2(image)
+    shift_image = np.fft.fftshift(fft_image)
+    dft_image = np.uint8(np.log(np.absolute(shift_image)) * 10)
+    if filtertype=='Butterworth Low Pass':
+        mask=get_butterworth_low_pass_filter(s,cutoff,order,width)
+    else:
+        mask=get_butterworth_high_pass_filter(s,cutoff,order,width)
 
+    filter_image = shift_image * mask
+    filter_finalimg = np.uint8(np.log(np.absolute(filter_image)) * 10)
 
-#############################Filter type#############################################
-#label for filter type
-filterType= Label(root,text="Filter Type",bg='white', fg="black")
-filterType.place(x=330,y=505)
-# Create a Tkinter variable Filter
-tkVarFilter = StringVar(root)
+    ishift_image = np.fft.ifftshift(filter_image)
+    ifft_image = np.fft.ifft2(ishift_image)
+    mag_image = np.absolute(ifft_image)
+    f = post_process_image(mag_image)
 
-# Dictionary for filter
-filters = {
-    'Normal Filter',
-    'Band Filter',
-    }
-tkVarFilter.set('None') #sets to default
-
-#Drop down gui for filter
-popupMenuFilter = OptionMenu(root,tkVarFilter,*filters)
-popupMenuFilter.place(x=300,y=525)
-
-#cutoff lable
-cutOff = Label(root, text="Cutoff", fg="black")
-cutOff.place(x=565, y=500)
-
-#cutoff user input
-cutOffEntry = Entry(root,width=10)
-cutOffEntry.place(x=615,y=500)
-cutOffEntry.insert(0,'50')
-#function for what each type of drop down will do
-def change_dropdown(self,*args):
-    if tkVarFilter.get() == 'Normal Filter':
-        Width = Label(root, text="                                       ")
-        Width.place(x=565, y=550)
-
-    if tkVarFilter.get() == 'Band Filter':
-        Width = Label(root, text="Width", fg="black")
-        Width.place(x=565, y=550)
-        change_dropdown.WidthEntry = Entry(root, width=10)
-        change_dropdown.WidthEntry.place(x=615, y=550)
-        change_dropdown.WidthEntry.insert(0, '10')
-
-
-convertImage = Button(root, text="Band Notch", bg="white", fg="black", command=notch)
-convertImage.place(x=565, y=250)
-# link function to change filter dropdown
-tkVarFilter.trace('w', change_dropdown)
-##################################Pass Type####################################################
-#print(tkVarFilter.get())
-# Create a Tkinter variable pass
-tkVarPass = StringVar(root)
-# Dictionary for the passes
-passes = {
-    'Ideal Low Pass',
-    'Ideal High Pass',
-    'Butterworth Low Pass',
-    'Butterworth High Pass',
-    'Gaussain Low Pass',
-    'Gaussian High Pass'}
-tkVarPass.set('None                  ') #sets to default
-
-#Drop down gui for passes
-popupMenuPass = OptionMenu(root,tkVarPass,*passes)
-popupMenuPass.place(x=300,y=550)
-
-#function for what each type of drop down will do
-def change_dropdown(*args):
-    if tkVarPass.get() == 'Butterworth Low Pass':
-        print("in")
-        lowInput= None
-        order = Label(root,text="Order ",fg="black")
-        order.place(x=565,y=525)
-        #places an input box for the user
-        change_dropdown.orderEntry = Entry(root,width=10)
-        change_dropdown.orderEntry.place(x=615, y=525)
-        change_dropdown.orderEntry.insert(0, '2')
-        #print(orderEntry.get())
-    if tkVarPass.get() == 'Butterworth High Pass':
-        order = Label(root,text="Order ", fg="black")
-        order.place(x=565,y=525)
-        # places an input box for the user
-        change_dropdown.orderEntry = Entry(root,width=10)
-        change_dropdown.orderEntry.place(x=615, y=525)
-        change_dropdown.orderEntry.insert(0,'2')
-    #from here on these passes will display over the Order label and user entry to block
-    if tkVarPass.get() == 'Gaussain Low Pass':
-        order = Label(root,text="                                        ")
-        order.place(x=565,y=525)
-    if tkVarPass.get() == 'Gaussian High Pass':
-        order = Label(root,text="                                        ")
-        order.place(x=565,y=525)
-    if tkVarPass.get() == 'Ideal High Pass':
-        order = Label(root, text="                                       ")
-        order.place(x=565, y=525)
-    if tkVarPass.get() == 'Ideal Low Pass':
-        order = Label(root, text="                                       ")
-        order.place(x=565, y=525)
-# link function to change pass dropdown
-tkVarPass.trace('w', change_dropdown)
-##############################################################################
-
-root.mainloop()
+    return [f,filter_finalimg]
